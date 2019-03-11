aed3064d15f5df8f0c4d4d64682696db492d3381
Luke Bakker <bakker.l@husky.neu.edu>
2019-03-11 15:41:20 -0400

    almost done

--- src/HomePage.java
+++ src/HomePage.java
@@ -1,15 +1,19 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 class HomePage extends JFrame {
   Container container;
+  private JScrollPane scrollPanel;
   private JLabel dolphinsVBengalsLabel, jetsVPatriotsLabel, cowboysVDolphinsLabel,
-    jetsVRamsLabel, patriotsVRamsLabel;
+    jetsVRamsLabel, patriotsVRamsLabel, myGroupsLabel;
   private JButton upload, searchButton, dolphinsVBengalsButton, jetsVPatriotsButton,
-          cowboysVDolphinsButton, jetsVRamsButton, patriotsVRamsButton;
+          cowboysVDolphinsButton, jetsVRamsButton, patriotsVRamsButton, createGroupButton, dolphinsGroup,
+  patriotsGroup, jetsGroup, ramsGroup;
   private JTextField searchBar;
-  private JPanel resultPanel;
+  private JPanel resultPanel, groupMenu;
 
   HomePage(String title) {
     super(title);
@@ -21,11 +25,78 @@ class HomePage extends JFrame {
     searchButton = new JButton("Search:");
     searchButton.setFont(new Font("Serif", Font.BOLD, 24));
 
-    ImageIcon cowboysVDolphins = new ImageIcon("../images/CowboysVDolphins.jpg");
-    ImageIcon dolphinsVBengals = new ImageIcon("../images/DolphinsVBengals.jpeg");
-    ImageIcon jetsVPatriots = new ImageIcon("../images/JetsVPatriots.jpeg");
-    ImageIcon jetsVRams = new ImageIcon("./JetsVRams.jpg");
-    ImageIcon patriotsVRams = new ImageIcon("../images/PatriotsVRams.jpeg");
+    makeSearchResults();
+    makeGroupMenu();
+    createGroupButton.setFont(new Font("Serif", Font.BOLD, 24));
+    jetsGroup.setFont(new Font("Serif", Font.BOLD, 24));
+    patriotsGroup.setFont(new Font("Serif", Font.BOLD, 24));
+    dolphinsGroup.setFont(new Font("Serif", Font.BOLD, 24));
+    ramsGroup.setFont(new Font("Serif", Font.BOLD, 24));
+
+
+
+
+
+    container = getContentPane();
+    setLocation();
+    setResultPanelProperties();
+    addComponents();
+
+
+    createGroupButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        JFrame addGroup = new CreateGroup("Create Group");
+        addGroup.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addGroup.setLocation(700, 150);
+        addGroup.setSize(new Dimension(200,150));
+        addGroup.setVisible(true);
+        ((CreateGroup) addGroup).addDoneListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            addRamsButton();
+          }
+        });
+      }
+    });
+    searchButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        resultPanel.remove(jetsVPatriotsButton);
+        resultPanel.remove(jetsVPatriotsLabel);
+        resultPanel.remove(dolphinsVBengalsButton);
+        resultPanel.remove(dolphinsVBengalsLabel);
+        resultPanel.remove(cowboysVDolphinsButton);
+        resultPanel.remove(cowboysVDolphinsLabel);
+        resultPanel.remove(jetsVRamsButton);
+        resultPanel.remove(jetsVRamsLabel);
+        resultPanel.setVisible(false);
+        resultPanel.setVisible(true);
+
+      }
+    });
+  }
+
+
+  private void makeGroupMenu() {
+    groupMenu = new JPanel();
+    groupMenu.setLayout(new BoxLayout(groupMenu, BoxLayout.PAGE_AXIS));
+    createGroupButton = new JButton("Create Group");
+    myGroupsLabel = new JLabel("My Groups:");
+    myGroupsLabel.setFont(new Font("Serif", Font.BOLD, 24));
+    dolphinsGroup = new JButton("Dolphins");
+    jetsGroup = new JButton("Jets");
+    patriotsGroup = new JButton("Patriots");
+    ramsGroup = new JButton("Rams");
+
+  }
+
+  private void makeSearchResults() {
+    ImageIcon cowboysVDolphins = new ImageIcon("./images/CowboysVDolphins.jpg");
+    ImageIcon dolphinsVBengals = new ImageIcon("./images/DolphinsVBengals.jpeg");
+    ImageIcon jetsVPatriots = new ImageIcon("./images/JetsVPatriots.jpeg");
+    ImageIcon jetsVRams = new ImageIcon("./images/JetsVRams.jpg");
+    ImageIcon patriotsVRams = new ImageIcon("./images/PatriotsVRams.jpeg");
 
 
     cowboysVDolphinsButton = new JButton(cowboysVDolphins);
@@ -34,56 +105,104 @@ class HomePage extends JFrame {
     dolphinsVBengalsButton = new JButton(dolphinsVBengals);
     patriotsVRamsButton = new JButton(patriotsVRams);
 
-    dolphinsVBengalsLabel = new JLabel("Dolphins vs. Bengals");
+    cowboysVDolphinsLabel = new JLabel("Cowboys vs. Dolphins");
+    jetsVPatriotsLabel = new JLabel("Jets vs. Patriots");
     jetsVRamsLabel = new JLabel("Jets vs. Rams");
     dolphinsVBengalsLabel = new JLabel("Dolphins vs. Bengals");
-    jetsVPatriotsLabel = new JLabel("Jets vs. Patriots");
-    patriotsVRamsLabel = new JLabel("Patriots vs. Rams");
+    patriotsVRamsLabel = new JLabel("Patriots vs Rams");
 
     resultPanel = new JPanel();
-    resultPanel.setLayout(new GridLayout(0,2));
-
-
-    container = getContentPane();
-
-    setLocation();
-    setResultPanelProperties();
-    addComponents();
+    resultPanel.setLayout(new BoxLayout(resultPanel, BoxLayout.PAGE_AXIS));
+    scrollPanel = new JScrollPane(resultPanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+            JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+    scrollPanel.setPreferredSize(new Dimension(600,600));
+    scrollPanel.setBounds(0, 150, 700, 600);
   }
 
   private void setLocation() {
     upload.setBounds(30,50,200,50);
     searchBar.setBounds(330,50,350,50);
-    resultPanel.setBounds(0, 150, 700, 800);
+    resultPanel.setBounds(0, 150, 700, 600);
     searchButton.setBounds(230,50,100,50);
-
+    groupMenu.setBounds(700, 0, 300, 800);
+    createGroupButton.setMaximumSize(new Dimension(200,50));
+    jetsGroup.setMaximumSize(new Dimension(200,50));
+    patriotsGroup.setMaximumSize(new Dimension(200,50));
+    dolphinsGroup.setMaximumSize(new Dimension(200,50));
 
   }
 
   private void addComponents() {
-    resultPanel.add(cowboysVDolphinsButton);
-    resultPanel.add(dolphinsVBengalsButton);
-    resultPanel.add(jetsVPatriotsButton);
-    resultPanel.add(jetsVRamsButton);
-    resultPanel.add(patriotsVRamsButton);
-    resultPanel.add(cowboysVDolphinsLabel);
+
+    // search results section
+
+    dolphinsVBengalsLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
     resultPanel.add(dolphinsVBengalsLabel);
+
+    dolphinsVBengalsButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+    resultPanel.add(dolphinsVBengalsButton);
+
+    cowboysVDolphinsLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+    resultPanel.add(cowboysVDolphinsLabel);
+
+    cowboysVDolphinsButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+    resultPanel.add(cowboysVDolphinsButton);
+
+    jetsVPatriotsLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
     resultPanel.add(jetsVPatriotsLabel);
+
+    jetsVPatriotsButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+    resultPanel.add(jetsVPatriotsButton);
+
+    jetsVRamsLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
     resultPanel.add(jetsVRamsLabel);
+
+    jetsVRamsButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+    resultPanel.add(jetsVRamsButton);
+
+    patriotsVRamsLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
     resultPanel.add(patriotsVRamsLabel);
 
+    patriotsVRamsButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+    resultPanel.add(patriotsVRamsButton);
 
+    // groups section
+
+    createGroupButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+    groupMenu.add(Box.createRigidArea(new Dimension(200,50)));
+    groupMenu.add(createGroupButton);
+    groupMenu.add(Box.createRigidArea(new Dimension(200,50)));
+    myGroupsLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+    groupMenu.add(myGroupsLabel);
+    groupMenu.add(Box.createRigidArea(new Dimension(200,50)));
+    jetsGroup.setAlignmentX(Component.CENTER_ALIGNMENT);
+    groupMenu.add(jetsGroup);
+    patriotsGroup.setAlignmentX(Component.CENTER_ALIGNMENT);
+    groupMenu.add(patriotsGroup);
+    dolphinsGroup.setAlignmentX(Component.CENTER_ALIGNMENT);
+    groupMenu.add(dolphinsGroup);
 
 
     container.add(upload);
+    container.add(scrollPanel);
     container.add(searchBar);
     container.add(searchButton);
-    container.add(resultPanel);
-
+    container.add(groupMenu);
   }
 
   private void setResultPanelProperties(){
     resultPanel.setBackground(Color.LIGHT_GRAY);
+    groupMenu.setBackground(Color.GRAY);
+  }
+
+  private void addRamsButton() {
+    System.out.println("HERE");
+    ramsGroup.setAlignmentX(Component.CENTER_ALIGNMENT);
+    ramsGroup.setMaximumSize(new Dimension(200,50));
+    groupMenu.add(ramsGroup);
+    groupMenu.setVisible(false);
+    groupMenu.setVisible(true);
+
   }
 
 }
